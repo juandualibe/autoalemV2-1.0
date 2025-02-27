46587f5e3d0f615d2531546c15e38e28fcfefc05
juandualibe <juandualibe@gmail.com>
2025-02-27 02:57:27 -0300

    Afianzando diseño login

--- hash.js
+++ hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const password = 'Duju1593!';
+const password = '123456';
 bcrypt.hash(password, 10, (err, hash) => {
   if (err) throw err;
   console.log(hash); // Copia este valor
